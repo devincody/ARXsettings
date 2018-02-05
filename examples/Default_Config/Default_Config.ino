382b4ca0ba7ea190c39bd2891078a166c7c88f60
devincody <devincody@gmail.com>
2018-02-05 15:58:53 -0800

    Update Default_Config.ino

--- examples/Default_Config/Default_Config.ino
+++ examples/Default_Config/Default_Config.ino
@@ -17,8 +17,8 @@ void setup(){
   Serial.begin(9600);                          //Must be included for ARXsetting::print_all() to work
   brd1.init();                                 //Initialize SPI and send IO intialization commands
   brd1.setAt1_all(14);                         //Set the value of At1 for all channels
-  brd1.setAt2_all(14);                         //Set the value of At1 for all channels
-  brd1.setAts_all(30);                         //Set the value of At1 for all channels
+  brd1.setAt2_all(14);                         //Set the value of At2 for all channels
+  brd1.setAts_all(30);                         //Set the value of At3 for all channels
   brd1.setFilter_all(ARXsetting::SPLIT);       //Set the Filter to SPLIT for all channels
   brd1.setFEE_all(false);                      //Set the FEE setting to OFF
   brd1.sendReg_all();                          //Send the control bits to the ARX board via SPI
