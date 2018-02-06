3ba146589ba37c623870061a11a747b4dc70ec1c
devincody <devincody@gmail.com>
2018-02-05 16:00:15 -0800

    Update Toggle_single.ino

--- examples/Toggle_single/Toggle_single.ino
+++ examples/Toggle_single/Toggle_single.ino
@@ -20,8 +20,8 @@ void setup(){
   Serial.begin(9600);                          //Must be included for ARXsetting::print_all() to work
   brd1.init();                                 //Initialize SPI and send IO intialization commands
   brd1.setAt1_all(14);                         //Set the value of At1 for all channels
-  brd1.setAt2_all(14);                         //Set the value of At1 for all channels
-  brd1.setAts_all(30);                         //Set the value of At1 for all channels
+  brd1.setAt2_all(14);                         //Set the value of At2 for all channels
+  brd1.setAts_all(30);                         //Set the value of Ats for all channels
   brd1.setFilter_all(ARXsetting::SPLIT);       //Set the Filter to SPLIT for all channels
   brd1.setFEE_all(false);                      //Set the FEE setting to OFF
   brd1.sendReg_all();                          //Send the control bits to the ARX board via SPI
@@ -31,16 +31,16 @@ void setup(){
 //Loop function executes continuously
 void loop(){
   brd1.setAt1(CHANNEL, 14);                         //Set the value of At1 for only CHANNEL
-  brd1.setAt2(CHANNEL, 14);                         //Set the value of At1 for only CHANNEL
-  brd1.setAts(CHANNEL, 30);                         //Set the value of At1 for only CHANNEL
+  brd1.setAt2(CHANNEL, 14);                         //Set the value of At2 for only CHANNEL
+  brd1.setAts(CHANNEL, 30);                         //Set the value of Ats for only CHANNEL
   brd1.setFilter(CHANNEL, ARXsetting::SPLIT);       //Set the Filter to SPLIT for only CHANNEL
   brd1.setFEE(CHANNEL, false);                      //Set the FEE setting to OFF for only CHANNEL
   brd1.sendReg_all();                               //Send the control bits to the ARX board via SPI
   brd1.print_all();                                 //Prints a summary of the current ARX register settings
   delay(WAIT);
   brd1.setAt1(CHANNEL, 30);                         //Set the value of At1 for only CHANNEL
-  brd1.setAt2(CHANNEL, 30);                         //Set the value of At1 for only CHANNEL
-  brd1.setAts(CHANNEL, 0);                          //Set the value of At1 for only CHANNEL
+  brd1.setAt2(CHANNEL, 30);                         //Set the value of At2 for only CHANNEL
+  brd1.setAts(CHANNEL, 0);                          //Set the value of Ats for only CHANNEL
   brd1.setFilter(CHANNEL, ARXsetting::REDUCED);     //Set the Filter to SPLIT for only CHANNEL
   brd1.setFEE(CHANNEL, true);                       //Set the FEE setting to OFF for only CHANNEL
   brd1.sendReg_all();                               //Send the control bits to the ARX board via SPI
